dd5afb7a917d9125d73ae8d152884cf45e365a46
Jack McDowell <jack@mcdowell.tv>
2019-07-14 14:10:41 -0400

    Restructure everything

---
+++ BLUESPAWN/BLUESPAWN/src/BLUESPAWN.cpp
@@ -0,0 +1,16 @@
+#include "CollectInfo.h"
+#include "Output.h"
+#include "Registry.h"
+#include "Hunts.h"
+#include "FileSystem.h"
+
+using namespace std;
+
+
+int main()
+{
+	OutputComputerInformation();
+	GoHuntingATTACK();
+	GoHuntingWeakSecuritySettings();
+}
+
