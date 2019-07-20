9eb1765b2b3574760907d9e9bf6c5d99ec205a35
Jack McDowell <jack@mcdowell.tv>
2019-07-20 19:25:59 -0400

    Create sample hunt; create basics for logging reaction

--- BLUESPAWN/Hunt/Hunts/src/HuntRegister.cpp
+++ BLUESPAWN/Hunt/Hunts/src/HuntRegister.cpp
@@ -1,1 +1,41 @@
 #include "HuntRegister.h"
+
+void HuntRegister::RegisterHunt(Hunt* hunt){
+	vRegisteredHunts.emplace_back(hunt);
+
+	/*for(DWORD i = 1; i != 0; i <<= 1){
+		if(hunt->UsesTactics(i)){
+			mTactics[(Tactic::Tactic) i].emplace_back(hunt);
+		}
+
+		if(hunt->UsesSources(i)){
+			mDataSources[(DataSource::DataSource) i].emplace_back(hunt);
+		}
+
+		if(hunt->AffectsStuff(i)){
+			mAffectedThings[(AffectedThing::AffectedThing) i].emplace_back(hunt);
+		}
+	}*/
+}
+
+
+void HuntRegister::RunHunts(DWORD dwTactics, DWORD dwDataSource, DWORD dwAffectedThings, Scope& scope, Aggressiveness::Aggressiveness aggressiveness, Reaction* reaction){
+	//Iterate hunts, run those selected
+}
+
+void HuntRegister::RunHunt(Hunt& name, Scope& scope, Aggressiveness::Aggressiveness aggressiveness, Reaction* reaction){
+	switch(aggressiveness){
+	case Aggressiveness::Cursory:
+		if(reaction) name.ScanCursory(scope, reaction); else name.ScanCursory(scope);
+		break;
+	case Aggressiveness::Moderate:
+		if(reaction) name.ScanModerate(scope, reaction); else name.ScanModerate(scope);
+		break;
+	case Aggressiveness::Careful:
+		if(reaction) name.ScanCareful(scope, reaction); else name.ScanCareful(scope);
+		break;
+	case Aggressiveness::Aggressive:
+		if(reaction) name.ScanAggressive(scope, reaction); else name.ScanAggressive(scope);
+		break;
+	}
+}
