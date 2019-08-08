e9340843e75fddf034e73ee979ec5c14570ef355
Jack McDowell <jack@mcdowell.tv>
2019-08-08 17:05:38 -0400

    Clean up logging in registry, make minor improvements

--- BLUESPAWN/Hunt/Hunts/headers/hunts/RegistryHunt.hpp
+++ BLUESPAWN/Hunt/Hunts/headers/hunts/RegistryHunt.hpp
@@ -2,6 +2,8 @@
 #include "configuration/Registry.h"
 #include "reactions/Reaction.h"
 
+#include <algorithm>
+
 namespace Registry {
 	constexpr bool MATCH_BAD = false;
 	constexpr bool NO_MATCH_BAD = true;
@@ -10,8 +12,8 @@ namespace Registry {
 	inline bool CheckKey(RegistryKey key, T value, Reaction* reaction, bool bOnMatch = NO_MATCH_BAD){
 		bool equal = key.Get<T>() == value;
 
-		if(!equal && bOnMatch || equal && !bOnMatch){
-			LOG_WARNING("Potentially bad registry key " << key << " with value " << key.Get<T>());
+		if(!equal && bOnMatch == NO_MATCH_BAD || equal && bOnMatch == MATCH_BAD){
+			LOG_WARNING("Potentially bad registry key " << key << " with value \"" << key.Get<T>() << "\". Value should " << (bOnMatch ? "" : "not ") << "be \"" << value << "\"");
 
 			reaction->RegistryKeyIdentified(key);
 		} else {
@@ -30,13 +32,14 @@ namespace Registry {
 		bool good = true;
 
 		for(auto value : key.Get<REG_MULTI_SZ_T>()){
-			if(find(values.begin(), values.end(), value) == values.end()) {
+			bool inList = find(values.begin(), values.end(), value) != values.end();
+			if(inList && bOnMatch == MATCH_BAD || !inList && bOnMatch == NO_MATCH_BAD) {
 				LOG(Log::_LogCurrentSinks, Log::LogLevel::LogWarn, "Potentially malicious registry key value discovered - " << value << Log::endlog
 					<< "Registry key is " << key << Log::endlog);
 				good = false;
 			}
 		}
-		
+
 		if(!good){
 			reaction->RegistryKeyIdentified(key);
 		} else {
