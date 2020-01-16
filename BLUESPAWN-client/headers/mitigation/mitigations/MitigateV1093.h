ad0d7f391839c0d903bda4b1c3b21b10f93e27af
Jake Smith <jake@metactf.com>
2020-01-16 11:12:19 -0500

    Add mitigation for V-1093 - anonymous shares

---
+++ BLUESPAWN-client/headers/mitigation/mitigations/MitigateV1093.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "../Mitigation.h"
+#include <mitigation\MitigationRegister.h>
+
+namespace Mitigations {
+
+	/**
+	 * MitigateV1093 looks for anonymous shares that are not restricted
+	 * (V-1093, CCI-001090).
+	 */
+	class MitigateV1093 : public Mitigation {
+	public:
+		MitigateV1093(MitigationRegister& record);
+
+		virtual bool MitigationIsEnforced(SecurityLevel level) override;
+		virtual bool EnforceMitigation(SecurityLevel level) override;
+		virtual bool MitigationApplies() override;
+	};
+}
