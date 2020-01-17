721e811b654b76f2f4c3dfb620d0d92f519b2544
CalvinKrist <calvin.krist@yahoo.com>
2020-01-17 11:31:19 -0500

    Modified event printing to print entire event content

--- BLUESPAWN-client/src/util/eventlogs/EventLogs.cpp
+++ BLUESPAWN-client/src/util/eventlogs/EventLogs.cpp
@@ -1,5 +1,8 @@
 #include "util/eventlogs/EventLogs.h"
 
+const int SIZE_DATA = 4096;
+TCHAR XMLDataCurrent[SIZE_DATA];
+
 void queryevent(void)
 {
 	DWORD status = ERROR_SUCCESS;
@@ -124,135 +127,6 @@ cleanup:
 	return status;
 }
 
-DWORD PrintEventSystemData(EVT_HANDLE hEvent)
-{
-	DWORD status = ERROR_SUCCESS;
-	EVT_HANDLE hContext = NULL;
-	DWORD dwBufferSize = 0;
-	DWORD dwBufferUsed = 0;
-	DWORD dwPropertyCount = 0;
-	PEVT_VARIANT pRenderedValues = NULL;
-	WCHAR wsGuid[50];
-	LPWSTR pwsSid = NULL;
-	ULONGLONG ullTimeStamp = 0;
-	ULONGLONG ullNanoseconds = 0;
-	SYSTEMTIME st;
-	FILETIME ft;
-	DWORD EventID;
-	// Identify the components of the event that you want to render. In this case,
-	// render the system section of the event.
-	hContext = EvtCreateRenderContext(0, NULL, EvtRenderContextValues);
-	if (NULL == hContext)
-	{
-		wprintf(L"EvtCreateRenderContext failed with %lu\n", status = GetLastError());
-		goto cleanup;
-	}
-
-	// When you render the user data or system section of the event, you must specify
-	// the EvtRenderEventValues flag. The function returns an array of variant values 
-	// for each element in the user data or system section of the event. For user data
-	// or event data, the values are returned in the same order as the elements are 
-	// defined in the event. For system data, the values are returned in the order defined
-	// in the EVT_SYSTEM_PROPERTY_ID enumeration.
-	if (!EvtRender(hContext, hEvent, EvtRenderEventValues, dwBufferSize, pRenderedValues, &dwBufferUsed, &dwPropertyCount))
-	{
-		if (ERROR_INSUFFICIENT_BUFFER == (status = GetLastError()))
-		{
-			dwBufferSize = dwBufferUsed;
-			pRenderedValues = (PEVT_VARIANT)malloc(dwBufferSize);
-			if (pRenderedValues)
-			{
-				EvtRender(hContext, hEvent, EvtRenderEventValues, dwBufferSize, pRenderedValues, &dwBufferUsed, &dwPropertyCount);
-			}
-			else
-			{
-				wprintf(L"malloc failed\n");
-				status = ERROR_OUTOFMEMORY;
-				goto cleanup;
-			}
-		}
-
-		if (ERROR_SUCCESS != (status = GetLastError()))
-		{
-			wprintf(L"EvtRender failed with %d\n", GetLastError());
-			goto cleanup;
-		}
-	}
-
-	// Print the values from the System section of the element.
-	wprintf(L"Provider Name: %s\n", pRenderedValues[EvtSystemProviderName].StringVal);
-	if (NULL != pRenderedValues[EvtSystemProviderGuid].GuidVal)
-	{
-		StringFromGUID2(*(pRenderedValues[EvtSystemProviderGuid].GuidVal), wsGuid, sizeof(wsGuid) / sizeof(WCHAR));
-		wprintf(L"Provider Guid: %s\n", wsGuid);
-	}
-	else
-	{
-		wprintf(L"Provider Guid: NULL");
-	}
-
-	EventID = pRenderedValues[EvtSystemEventID].UInt16Val;
-	
-	if (EvtVarTypeNull != pRenderedValues[EvtSystemQualifiers].Type)
-	{
-		EventID = MAKELONG(pRenderedValues[EvtSystemEventID].UInt16Val, pRenderedValues[EvtSystemQualifiers].UInt16Val);
-	}
-	wprintf(L"EventID: %lu\n", EventID);
-
-	wprintf(L"Version: %u\n", (EvtVarTypeNull == pRenderedValues[EvtSystemVersion].Type) ? 0 : pRenderedValues[EvtSystemVersion].ByteVal);
-	wprintf(L"Level: %u\n", (EvtVarTypeNull == pRenderedValues[EvtSystemLevel].Type) ? 0 : pRenderedValues[EvtSystemLevel].ByteVal);
-	wprintf(L"Task: %hu\n", (EvtVarTypeNull == pRenderedValues[EvtSystemTask].Type) ? 0 : pRenderedValues[EvtSystemTask].UInt16Val);
-	wprintf(L"Opcode: %u\n", (EvtVarTypeNull == pRenderedValues[EvtSystemOpcode].Type) ? 0 : pRenderedValues[EvtSystemOpcode].ByteVal);
-	wprintf(L"Keywords: 0x%I64x\n", pRenderedValues[EvtSystemKeywords].UInt64Val);
-
-	ullTimeStamp = pRenderedValues[EvtSystemTimeCreated].FileTimeVal;
-	ft.dwHighDateTime = (DWORD)((ullTimeStamp >> 32) & 0xFFFFFFFF);
-	ft.dwLowDateTime = (DWORD)(ullTimeStamp & 0xFFFFFFFF);
-
-	FileTimeToSystemTime(&ft, &st);
-	ullNanoseconds = (ullTimeStamp % 10000000) * 100; // Display nanoseconds instead of milliseconds for higher resolution
-	wprintf(L"TimeCreated SystemTime: %02d/%02d/%02d %02d:%02d:%02d.%I64u)\n",
-		st.wMonth, st.wDay, st.wYear, st.wHour, st.wMinute, st.wSecond, ullNanoseconds);
-
-	wprintf(L"EventRecordID: %I64u\n", pRenderedValues[EvtSystemEventRecordId].UInt64Val);
-
-	if (EvtVarTypeNull != pRenderedValues[EvtSystemActivityID].Type)
-	{
-		StringFromGUID2(*(pRenderedValues[EvtSystemActivityID].GuidVal), wsGuid, sizeof(wsGuid) / sizeof(WCHAR));
-		wprintf(L"Correlation ActivityID: %s\n", wsGuid);
-	}
-
-	if (EvtVarTypeNull != pRenderedValues[EvtSystemRelatedActivityID].Type)
-	{
-		StringFromGUID2(*(pRenderedValues[EvtSystemRelatedActivityID].GuidVal), wsGuid, sizeof(wsGuid) / sizeof(WCHAR));
-		wprintf(L"Correlation RelatedActivityID: %s\n", wsGuid);
-	}
-
-	wprintf(L"Execution ProcessID: %lu\n", pRenderedValues[EvtSystemProcessID].UInt32Val);
-	wprintf(L"Execution ThreadID: %lu\n", pRenderedValues[EvtSystemThreadID].UInt32Val);
-	wprintf(L"Channel: %s\n", (EvtVarTypeNull == pRenderedValues[EvtSystemChannel].Type) ? L"" : pRenderedValues[EvtSystemChannel].StringVal);
-	wprintf(L"Computer: %s\n", pRenderedValues[EvtSystemComputer].StringVal);
-
-	if (EvtVarTypeNull != pRenderedValues[EvtSystemUserID].Type)
-	{
-		if (ConvertSidToStringSid(pRenderedValues[EvtSystemUserID].SidVal, &pwsSid))
-		{
-			wprintf(L"Security UserID: %s\n", pwsSid);
-			LocalFree(pwsSid);
-		}
-	}
-
-cleanup:
-
-	if (hContext)
-		EvtClose(hContext);
-
-	if (pRenderedValues)
-		free(pRenderedValues);
-
-	return status;
-}
-
 DWORD PrintEventValues(EVT_HANDLE hEvent)
 {
 	DWORD status = ERROR_SUCCESS;
@@ -263,31 +137,32 @@ DWORD PrintEventValues(EVT_HANDLE hEvent)
 	PEVT_VARIANT pRenderedValues = NULL;
 	LPWSTR ppValues[] = { L"Event/System/Computer" };
 	DWORD count = sizeof(ppValues) / sizeof(LPWSTR);
+	LPWSTR pRenderedContent = NULL;
 
 	// Identify the components of the event that you want to render. In this case,
 	// render the provider's name and channel from the system section of the event.
 	// To get user data from the event, you can specify an expression such as
 	// L"Event/EventData/Data[@Name=\"<data name goes here>\"]".
-	hContext = EvtCreateRenderContext(count, (LPCWSTR*)ppValues, EvtRenderContextValues);
 	//hContext = EvtCreateRenderContext(count, (LPCWSTR*)ppValues, EvtRenderContextValues);
-	if (NULL == hContext)
+	//hContext = EvtCreateRenderContext(count, (LPCWSTR*)ppValues, EvtRenderContextValues);
+	/*if (NULL == hContext)
 	{
 		wprintf(L"EvtCreateRenderContext failed with %lu\n", status = GetLastError());
 		goto cleanup;
-	}
+	}*/
 
 	// The function returns an array of variant values for each element or attribute that
 	// you want to retrieve from the event. The values are returned in the same order as 
 	// you requested them.
-	if (!EvtRender(hContext, hEvent, EvtRenderEventValues, dwBufferSize, pRenderedValues, &dwBufferUsed, &dwPropertyCount))
+	if (!EvtRender(NULL, hEvent, EvtRenderEventXml, dwBufferSize, pRenderedContent, &dwBufferUsed, &dwPropertyCount))
 	{
 		if (ERROR_INSUFFICIENT_BUFFER == (status = GetLastError()))
 		{
 			dwBufferSize = dwBufferUsed;
-			pRenderedValues = (PEVT_VARIANT)malloc(dwBufferSize);
-			if (pRenderedValues)
+			pRenderedContent = (LPWSTR)malloc(dwBufferSize);
+			if (pRenderedContent)
 			{
-				EvtRender(hContext, hEvent, EvtRenderEventValues, dwBufferSize, pRenderedValues, &dwBufferUsed, &dwPropertyCount);
+				EvtRender(NULL, hEvent, EvtRenderEventXml, dwBufferSize, pRenderedContent, &dwBufferUsed, &dwPropertyCount);
 			}
 			else
 			{
@@ -305,7 +180,11 @@ DWORD PrintEventValues(EVT_HANDLE hEvent)
 	}
 
 	// Print the selected values.
-	wprintf(L"\nProvider Name: %s\n", pRenderedValues[0].StringVal);
+	//wprintf(L"\nProvider Name: %s\n", pRenderedValues[0].StringVal);
+	ZeroMemory(XMLDataCurrent, SIZE_DATA);
+	lstrcpyW(XMLDataCurrent, pRenderedContent);
+
+	wprintf(L"EvtRender data %s\n", XMLDataCurrent);
 
 cleanup:
 
