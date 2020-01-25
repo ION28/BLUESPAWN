55fda2fb510d4ea2fd0d9139ef06df8d6d1cba78
wtm99 <willmayes1999@gmail.com>
2020-01-25 13:25:49 -0500

    Basic File handling

--- BLUESPAWN-client/src/util/filesystem/FileSystem.cpp
+++ BLUESPAWN-client/src/util/filesystem/FileSystem.cpp
@@ -1,5 +1,5 @@
-#include "C:\\Users\\Will Mayes\\Documents\\Cyber Security\\BLUESPAWN\\BLUESPAWN-client\\headers\\util\\filesystem\\FileSystem.h"
-//#include "util/filesystem/FileSystem.h"
+//#include "C:\\Users\\Will Mayes\\Documents\\Cyber Security\\BLUESPAWN\\BLUESPAWN-client\\headers\\util\\filesystem\\FileSystem.h"
+#include "util/filesystem/FileSystem.h"
 bool FileSystem::CheckFileExists(LPCWSTR filename) {
 	//Function from https://stackoverflow.com/a/4404259/3302799
 	GetFileAttributesW(filename);
@@ -10,13 +10,21 @@ bool FileSystem::CheckFileExists(LPCWSTR filename) {
 	}
 	return true;
 }
-//string GetFileContents(LPCWSTR);
-//bool HashFileMD5(LPCWSTR, string&);
+
+void FileSystem::File::TranslateLongToFilePointer(long val, LONG& lowerVal, LONG& upperVal, PLONG& upper) {
+	//Calculate the offset into format needed for SetFilePointer
+	long lowerMask = 0xFFFFFFFF;
+	long upperMask = (long)(0x7FFFFFFF) << 32;
+	lowerVal = val & lowerMask;
+	upperVal = val & upperMask;
+	upper = &upperVal;
+}
+
 FileSystem::File::File(IN const LPCWSTR path) {
 	FilePath = path;
 	hFile = CreateFileW(path, 
 		GENERIC_READ | GENERIC_WRITE, 
-		0, 
+		FILE_SHARE_READ,
 		NULL, 
 		OPEN_EXISTING,
 		FILE_FLAG_SEQUENTIAL_SCAN | FILE_ATTRIBUTE_NORMAL,
@@ -34,26 +42,98 @@ FileSystem::File::File(IN const LPCWSTR path) {
 }
 
 short FileSystem::File::Write(IN const LPVOID value, IN const long offset, IN const unsigned long length, IN const bool insert) {
-	//TODO: Add ability to insert and handle writing past end of file. 
 	if (!FileExists) return 0;
-	long lowerMask = 0xFFFFFFFF;
-	long upperMask = (long)0x7FFFFFFF << 32;
-	LONG lowerOffset = offset & lowerMask;
-	LONG upperOffset = offset & upperMask;
+	//Calculate the offset into format needed for SetFilePointer
+	LONG lowerOffset;
+	LONG upperOffset;
 	PLONG upper;
+	File::TranslateLongToFilePointer(offset, lowerOffset, upperOffset, upper);
 	DWORD bytesWritten;
-	if (upperOffset) {
-		upper = &upperOffset;
-	}
-	else {
-		upper = NULL;
-	}
+	//Point at the desired offset
 	if (SetFilePointer(hFile, lowerOffset, upper, 0) == INVALID_SET_FILE_POINTER) {
 		return 0;
 	}
-	if (!WriteFile(hFile, value, length, &bytesWritten, NULL)) {
-		SetFilePointer(hFile, 0, 0, 0);
-		return 0;
+	//Insert value into file at specified offset
+	if (insert) {
+		bool readAll = false;
+		long readOffset = offset;
+		long writeOffset = offset;
+		int i = 0;
+		int writeLen = length;
+		LPVOID writeBuf = value;
+		PLONG writeUpper;
+		LONG writeLowerOffset;
+		LONG writeUpperOffset;
+		File::TranslateLongToFilePointer(writeOffset, writeLowerOffset, writeUpperOffset, writeUpper);
+		//Read up to 1000000 bytes at a time until eof is reached, writing the last read chunk between each read. 
+		while (!readAll) {
+			LPVOID readBuf = calloc(writeLen + 1, 1);
+			DWORD bytesRead;
+			long len = writeLen;
+			//Read to eof file or ~1000000 bytes
+			while (true) {
+				if (SetFilePointer(hFile, lowerOffset, upper, 0) == INVALID_SET_FILE_POINTER) {
+					return 0;
+				}
+				if (!ReadFile(hFile, readBuf, len, &bytesRead, NULL)) {
+					free(readBuf);
+					SetFilePointer(hFile, 0, 0, 0);
+					return 0;
+				}
+				if (bytesRead < len) {
+					readAll = true;
+					break;
+				}
+				if (len * 2 > 1000000) {
+					break;
+				}
+				free(readBuf);
+				len *= 2;
+				readBuf = calloc(len * 2 + 1, 1);
+				if (!readBuf) {
+					return 0;
+				}
+//				std::cout << "HERE " << len << std::endl;
+			}
+			//Write last read portion of the file
+			if (SetFilePointer(hFile, writeLowerOffset, writeUpper, 0) == INVALID_SET_FILE_POINTER) {
+				return 0;
+			}
+			if (!WriteFile(hFile, writeBuf, writeLen, &bytesWritten, NULL)) {
+				free(readBuf);
+				if (i > 0) free(writeBuf);
+				SetFilePointer(hFile, 0, 0, 0);
+				return 0;
+			}
+			//Free last write unless it was the given buffer
+			if(i > 0) free(writeBuf);
+			//Store the last read in prepartion for next write
+			writeBuf = readBuf;
+			writeLen = bytesRead;
+			//Update the read and write offsets
+			writeOffset += bytesWritten;
+			File::TranslateLongToFilePointer(writeOffset, writeLowerOffset, writeUpperOffset, writeUpper);
+			readOffset += bytesRead;
+			File::TranslateLongToFilePointer(readOffset, lowerOffset, upperOffset, upper);
+			i++;
+		}
+		if (SetFilePointer(hFile, writeLowerOffset, writeUpper, 0) == INVALID_SET_FILE_POINTER) {
+			free(writeBuf);
+			return 0;
+		}
+		if (!WriteFile(hFile, writeBuf, writeLen, &bytesWritten, NULL)) {
+			free(writeBuf);
+			SetFilePointer(hFile, 0, 0, 0);
+			return 0;
+		}
+		free(writeBuf);
+	}
+	//Write value over file at specified offset
+	else {
+		if (!WriteFile(hFile, value, length, &bytesWritten, NULL)) {
+			SetFilePointer(hFile, 0, 0, 0);
+			return 0;
+		}
 	}
 	SetFilePointer(hFile, 0, 0, 0);
 	return 1;
@@ -61,17 +141,11 @@ short FileSystem::File::Write(IN const LPVOID value, IN const long offset, IN co
 
 short FileSystem::File::Read(OUT LPVOID buffer, IN const long offset, IN const unsigned long amount) {
 	if (!FileExists) return 0;
-	long lowerMask = 0xFFFFFFFF;
-	long upperMask = (long)0x7FFFFFFF << 32;
-	LONG lowerOffset = offset & lowerMask;
-	LONG upperOffset = offset & upperMask;
+	//Calculate the offset into format needed for SetFilePointer
+	LONG lowerOffset;
+	LONG upperOffset;
 	PLONG upper;
-	if (upperOffset) {
-		upper = &upperOffset;
-	}
-	else {
-		upper = NULL;
-	}
+	File::TranslateLongToFilePointer(offset, lowerOffset, upperOffset, upper);
 	DWORD bytesRead;
 	if (SetFilePointer(hFile, lowerOffset, upper, 0) == INVALID_SET_FILE_POINTER) {
 		return 0;
@@ -166,11 +240,11 @@ bool FileSystem::File::GetMD5Hash(OUT string& buffer) {
 	return false;
 }
 
-short FileSystem::File::MakeFile() {
+short FileSystem::File::Create() {
 	if (FileExists) return 0;
 	hFile = CreateFileW(FilePath,
 		GENERIC_READ | GENERIC_WRITE,
-		0,
+		FILE_SHARE_READ,
 		NULL,
 		CREATE_NEW,
 		FILE_FLAG_SEQUENTIAL_SCAN | FILE_ATTRIBUTE_NORMAL,
@@ -186,7 +260,7 @@ short FileSystem::File::MakeFile() {
 	return 1;
 }
 
-short FileSystem::File::RemoveFile() {
+short FileSystem::File::Delete() {
 	if (!FileExists) return 0;
 	CloseHandle(hFile);
 	if (!DeleteFileW(FilePath)) {
@@ -211,4 +285,16 @@ short FileSystem::File::RemoveFile() {
 	}
 	FileExists = false;
 	return 1;
+}
+
+short FileSystem::File::ChangeFileLength(IN const long length) {
+	//Calculate the length into format needed for SetFilePointer
+	LONG lowerLen;
+	LONG upperLen;
+	PLONG upper;
+	File::TranslateLongToFilePointer(length, lowerLen, upperLen, upper);
+	if (!SetFilePointer(hFile, lowerLen, upper, 0)) return 0;
+	if (!SetEndOfFile(hFile)) return 0;
+	return 1;
+
 }
