611294de69808e135df74893cfee925cb7aa4cad
Jack <jack@mcdowell.tv>
2020-02-03 15:01:15 -0500

    Mitigate SMBv1

--- BLUESPAWN-client/headers/mitigation/mitigations/MitigateV73519.h
+++ BLUESPAWN-client/headers/mitigation/mitigations/MitigateV73519.h
@@ -4,14 +4,14 @@
 #include "hunt/reaction/Reaction.h"
 #include "hunt/reaction/Log.h"
 
-namespace Mitigations {
+namespace Mitigations{
 
 	/**
 	 * MitigateV72753 looks for Wdigest authentication to be disabled. (V-72753).
 	 */
-	class MitigateV72753 : public Mitigation {
+	class MitigateV73519 : public Mitigation {
 	public:
-		MitigateV72753(MitigationRegister& record);
+		MitigateV73519(MitigationRegister& record);
 
 		virtual bool MitigationIsEnforced(SecurityLevel level) override;
 		virtual bool EnforceMitigation(SecurityLevel level) override;
