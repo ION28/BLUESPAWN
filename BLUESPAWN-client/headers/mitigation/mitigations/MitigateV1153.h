441f9587d1d7c46230a55515b82de34328c89a97
Jack <jack@mcdowell.tv>
2020-02-04 10:40:43 -0500

    Create a mitigation for V-1153 (LM Compaitibility Level)

---
+++ BLUESPAWN-client/headers/mitigation/mitigations/MitigateV1153.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "../Mitigation.h"
+#include <mitigation\MitigationRegister.h>
+
+namespace Mitigations{
+
+	/**
+	 * MitigateV1153 ensures NTLMv2 is used
+	 * (V-1093, CCI-001090).
+	 */
+	class MitigateV1153 : public Mitigation {
+	public:
+		MitigateV1153(MitigationRegister& record);
+
+		virtual bool MitigationIsEnforced(SecurityLevel level) override;
+		virtual bool EnforceMitigation(SecurityLevel level) override;
+		virtual bool MitigationApplies() override;
+	};
+}
