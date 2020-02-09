97df213d5e99a94353311ced6f71b2e0f7ca2d48
Jack <jack@mcdowell.tv>
2020-02-08 20:26:10 -0500

    Add Mitigation for V3379

---
+++ BLUESPAWN-client/headers/mitigation/mitigations/MitigateV3379.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "../Mitigation.h"
+#include <mitigation\MitigationRegister.h>
+#include "hunt/reaction/Reaction.h"
+#include "hunt/reaction/Log.h"
+
+namespace Mitigations{
+
+	/**
+	 * MitigateV3379 ensures that the LAN Manager does not store LM hashes in the 
+	 * SAM registry hive.
+	 */
+	class MitigateV3379 : public Mitigation {
+	public:
+		MitigateV3379(MitigationRegister& record);
+
+		virtual bool MitigationIsEnforced(SecurityLevel level) override;
+		virtual bool EnforceMitigation(SecurityLevel level) override;
+		virtual bool MitigationApplies() override;
+	};
+}
