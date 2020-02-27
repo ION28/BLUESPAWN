74a9ab5acb58558dd4ea0d941a7c1e39cabc9787
Jake Smith <jake@metactf.com>
2020-02-27 15:31:43 -0500

    Work on T1060 Updates

--- BLUESPAWN-client/src/hunt/hunts/HuntT1060.cpp
+++ BLUESPAWN-client/src/hunt/hunts/HuntT1060.cpp
@@ -12,13 +12,6 @@ namespace Hunts {
 		dwCategoriesAffected = (DWORD) Category::Configurations;
 		dwSourcesInvolved = (DWORD) DataSource::Registry;
 		dwTacticsUsed = (DWORD) Tactic::Persistence;
-	}
-
-	int HuntT1060::ScanCursory(const Scope& scope, Reaction reaction){
-		LOG_INFO(L"Hunting for " << name << L" at level Cursory");
-		reaction.BeginHunt(GET_INFO());
-
-		std::map<RegistryKey, std::vector<RegistryValue>> keys;
 
 		auto HKLMRun = RegistryKey{ HKEY_LOCAL_MACHINE, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run" };
 		auto HKCURun = RegistryKey{ HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run" };
@@ -34,49 +27,72 @@ namespace Hunts {
 		auto HKCURunOnceExWow64 = RegistryKey{ HKEY_CURRENT_USER, L"Software\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\RunOnceEx" };
 		auto HKLMExplorerRun = RegistryKey{ HKEY_LOCAL_MACHINE, L"Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run" };
 		auto HKCUExplorerRun = RegistryKey{ HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run" };
+		auto HKLMExplorerRunWow64 = RegistryKey{ HKEY_LOCAL_MACHINE, L"Software\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run" };
+		auto HKCUExplorerRunWow64 = RegistryKey{ HKEY_CURRENT_USER, L"Software\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run" };
 
-		std::vector<RegistryKey> RunKeys = {
+		RunKeys = {
 			HKLMRun, HKLMRunOnce, HKLMRunOnceEx,
 			HKCURun, HKCURunOnce, HKCURunOnceEx,
 			HKLMRunWow64, HKLMRunOnceWow64, HKLMRunOnceExWow64,
 			HKCURunWow64, HKCURunOnceWow64, HKCURunOnceExWow64,
 			HKLMExplorerRun, HKCUExplorerRun,
+			HKLMExplorerRunWow64, HKCUExplorerRunWow64,
 		};
-		
-		for(auto key : RunKeys){
-			keys.emplace(key, CheckKeyValues(key));
-		}
-
-		auto HKCUShell = RegistryKey{ HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders" };
-		keys.emplace(HKCUShell, CheckValues(HKCUShell, {
-		    { L"Startup", RegistryType::REG_EXPAND_SZ_T, L"%USERPROFILE%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup", false, CheckSzEqual }
-		}));
-
-		auto HKLMUShell = RegistryKey{ HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders" };
-		keys.emplace(HKLMUShell, CheckValues(HKLMUShell, {
-			{ L"Common Startup", RegistryType::REG_EXPAND_SZ_T, L"%ProgramData%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup", false, CheckSzEqual }
-		}));
-
-
-		auto HKLMShell = RegistryKey{ HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders" };
-		keys.emplace(HKLMShell, CheckValues(HKLMShell, {
-			{ L"Common Startup", RegistryType::REG_EXPAND_SZ_T, L"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup", false, CheckSzEqual }
-		}));
-
 
 		auto HKLMCMDRun = RegistryKey{ HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Command Processor" };
 		auto HKLMCMDRunWow64 = RegistryKey{ HKEY_CURRENT_USER, L"SOFTWARE\\WOW6432Node\\Microsoft\\Command Processor" };
 		auto HKCUCMDRun = RegistryKey{ HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Command Processor" };
 		auto HKCUCMDRunWow64 = RegistryKey{ HKEY_CURRENT_USER, L"SOFTWARE\\WOW6432Node\\Microsoft\\Command Processor" };
-		
-		std::vector<RegistryKey> CMDKeys = {
-			HKLMCMDRun, HKLMCMDRunWow64, HKCUCMDRun, HKCUCMDRunWow64
+
+		CMDKeys = {
+			HKLMCMDRun, HKLMCMDRunWow64, HKCUCMDRun, HKCUCMDRunWow64,
 		};
-		for (auto key : CMDKeys) {
+
+		auto HKCUUShell = RegistryKey{ HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders" };
+		auto HKLMUShell = RegistryKey{ HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders" };
+		auto HKCUShell = RegistryKey{ HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders" };
+		auto HKLMShell = RegistryKey{ HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders" };
+		auto HKCUUShellWow64 = RegistryKey{ HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders" };
+		auto HKLMUShellWow64 = RegistryKey{ HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders" };
+		auto HKCUShellWow64 = RegistryKey{ HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders" };
+		auto HKLMShellWow64 = RegistryKey{ HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders" };
+
+		ShellKeys = {
+			HKLMUShell, HKLMShell,  HKLMShellWow64, HKLMUShellWow64,
+		};
+
+		UserShellKeys = {
+			HKCUShell, HKCUUShell, HKCUShellWow64, HKCUUShellWow64,
+		};
+	}
+
+	int HuntT1060::ScanCursory(const Scope& scope, Reaction reaction){
+		LOG_INFO(L"Hunting for " << name << L" at level Cursory");
+		reaction.BeginHunt(GET_INFO());
+
+		std::map<RegistryKey, std::vector<RegistryValue>> keys;
+		
+		for(auto key : RunKeys){
+			keys.emplace(key, CheckKeyValues(key));
+		}
+
+		for(auto key : CMDKeys) {
 			keys.emplace(key, CheckValues(key, {
 				{ L"AutoRun", RegistryType::REG_SZ_T, L"", false, CheckSzEmpty }
 			}));
-		}		
+		}
+
+		for (auto key : UserShellKeys) {
+			keys.emplace(key, CheckValues(key, {
+				{ L"Startup", RegistryType::REG_EXPAND_SZ_T, L"%USERPROFILE%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup", false, CheckSzEqual }
+			}));
+		}
+
+		for (auto key : ShellKeys) {
+			keys.emplace(key, CheckValues(key, {
+				{ L"Common Startup", RegistryType::REG_EXPAND_SZ_T, L"%ProgramData%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup", false, CheckSzEqual }
+			}));
+		}
 
 		int detections = 0;
 		for(const auto& key : keys){
@@ -88,5 +104,14 @@ namespace Hunts {
 
 		reaction.EndHunt();
 		return detections;
+	}
+
+	std::vector<std::shared_ptr<Event>> HuntT1060::GetMonitoringEvents() {
+		std::vector<std::shared_ptr<Event>> events;
+		for (auto key : RunKeys) { events.push_back(std::make_shared<RegistryEvent>(key)); }
+		for (auto key : CMDKeys) { events.push_back(std::make_shared<RegistryEvent>(key)); }
+		for (auto key : ShellKeys) { events.push_back(std::make_shared<RegistryEvent>(key)); }
+		for (auto key : UserShellKeys) { events.push_back(std::make_shared<RegistryEvent>(key)); }
+		return events;
 	}
 }
