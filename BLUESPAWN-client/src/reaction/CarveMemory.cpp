508640c374b77e42805649c1017ed93777bc4c0d
Jake Smith <jake@metactf.com>
2020-03-28 14:27:31 -0400

    Change CmdLine to ImagePath in process detections

--- BLUESPAWN-client/src/reaction/CarveMemory.cpp
+++ BLUESPAWN-client/src/reaction/CarveMemory.cpp
@@ -8,7 +8,7 @@
 namespace Reactions {
 
 	void CarveProcessReaction::CarveProcessIdentified(std::shared_ptr<PROCESS_DETECTION> detection){
-		if(io.GetUserConfirm(detection->wsCmdline + L" appears to be infected. Suspend process?") == 1){
+		if(io.GetUserConfirm(detection->wsImagePath + L" (PID " + std::to_wstring(detection->PID) + L") appears to be infected. Carve out and terminate malicious memory section?") == 1){
 			auto remover = PERemover{ static_cast<DWORD>(detection->PID), detection->lpAllocationBase, detection->dwAllocationSize };
 			remover.RemoveImage();
 		}
