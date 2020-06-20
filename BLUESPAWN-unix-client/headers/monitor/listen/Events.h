f1419a60ab8cfd11ad7425d3bad018f46f932ae6
aaron <agski@earthlink.net>
2020-06-20 00:50:51 -0400

    More work setting up events framework

--- BLUESPAWN-unix-client/headers/monitor/listen/Events.h
+++ BLUESPAWN-unix-client/headers/monitor/listen/Events.h
@@ -2,6 +2,9 @@
 
 #include <atomic>
 #include <pthread.h>
+#include <optional>
+#include <string>
+#include <linux/ptrace.h>
 
 #include "util/ThreadsafeQueue.h"
 
@@ -18,24 +21,82 @@ class EventHandle{
 private:
     ThreadsafeQueue<EventDetails> signalQueue;
 
+    EventDetails details;
+
 public:
 
-    EventHandle();
-
-    ~EventHandle();
+    EventHandle(EventDetails details);
 
     bool operator==(const EventHandle& e) const;
+
+    bool HasSignal();
+
+    EventDetails PopSignal();
+
+    void PushSignal(EventDetails details);
+
+
 
 };
 
 //an implementation of WaitForMultipleObjects to reduce overhead in the eventsListener class
-int WaitForMultipleObjects(int nCount, const Events::EventHandle * lpHandles, bool bWaitAll, int dwMilliSeconds);
+int WaitForMultipleObjects(int nCount, Events::EventHandle * lpHandles, bool bWaitAll, int dwMilliSeconds);
 
 //for compatibility with the WaitForSingleObjects on the hManager event handle
 bool WaitForSingleObject(std::atomic<bool> &hHandle, int dwMilliseconds);
 //holds dynamic event details
-class EventDetails{
+enum class EventType{
+    SystemCall,
+    FileSystem,
+    ProcessSignal
+};
 
+class EventDetails{
+private:
+    EventType type;
+    //required
+    pid_t pid; //pid that spawned the event
+
+    time_t timestamp; //timestamp of the event
+    //system call specific options
+    std::optional<long> number;
+    std::optional<struct pt_regs> registers;
+
+    //filesystem specific
+    std::optional<std::string> path;
+    std::optional<mode_t> mode;
+
+    //process specific
+    std::optional<pid_t> on; //on fork events, its the child process spawned.  On signal events, its the process that was signaled.
+    std::optional<int> signal; //signal only
+public:
+    EventDetails(pid_t pid, long number, struct pt_regs * registers);
+
+    EventDetails(pid_t pid, std::string path, mode_t mode);
+
+    EventDetails(pid_t pid, pid_t on, int signal);
+
+    bool operator==(const EventDetails& other) const;
+
+    EventType GetType() const;
+
+    pid_t GetPid() const;
+
+    std::optional<long> GetSyscallNumber() const;
+
+    std::optional<struct pt_regs> GetRegisters() const;
+
+    std::optional<std::string> GetPath() const;
+
+    std::optional<mode_t> GetOperation() const;
+
+    std::optional<pid_t> GetChild() const;
+
+    std::optional<int> GetSignal() const;
+
+    time_t GetTimestamp() const;
+
+    ProcessEventType GetProcessEventType() const;
 };
 
 };
--- BLUESPAWN-unix-client/src/monitor/listen/Events.cpp
+++ BLUESPAWN-unix-client/src/monitor/listen/Events.cpp
@@ -1,17 +1,72 @@
 #include "Events.h"
 #include <unistd.h>
+#include <string.h>
 
 namespace Events{
 
     bool EventHandle::operator==(const EventHandle& e) const{
-        return true;
+        return this->details == e.details; //not sure if this method is actually needed
     }
 
+    EventHandle::EventHandle(EventDetails details) : details{ details }{}
 
-    int WaitForMultipleObjects(int nCount, const Events::EventHandle * lpHandles, bool bWaitAll, int dwMilliSeconds){
+    bool EventHandle::HasSignal() {
+        return !signalQueue.empty();
+    }
+
+    EventDetails EventHandle::PopSignal() {
+        return signalQueue.pop();
+    }
+
+    void EventHandle::PushSignal(EventDetails details){
+        signalQueue.push(details);
+    }
+
+    EventDetails::EventDetails(pid_t pid, long number, struct pt_regs * registers) : type{EventType::SystemCall}, pid{pid}, number{number}{
+        if(!registers){
+            memcpy(&this->registers, registers, sizeof(struct pt_regs));
+        }
+    }
+
+    EventDetails::EventDetails(pid_t pid, std::string path, mode_t mode) : type{EventType::FileSystem}, pid{pid}, path{path}, mode{mode}{}
+
+    EventDetails::EventDetails(pid_t pid, pid_t on, int signal) : type{EventType::ProcessSignal}, pid{pid}, on{on}, signal{signal}{}
+
+    bool EventDetails::operator==(const EventDetails & other) const {
+        if(type == EventType::FileSystem){
+            return path == other.path && mode == other.mode;
+        }else if(type == EventType::ProcessSignal){
+            return signal == other.signal;
+        }else if(type == EventType::SystemCall){
+            return number == other.number;
+        }
+    }
+
+    int WaitForMultipleObjects(int nCount, Events::EventHandle * lpHandles, bool bWaitAll, int dwMilliSeconds){
         //check through each handle
         //Again, dont really need the DW
+        bool * done = new bool[nCount];
+        int nDone = 0;
+        while(true){
+            for(int i = 0; i < nCount; i++){
+                if(!done[i] && lpHandles[i].HasSignal()){
+                    done[i] = true;
+                    nDone ++;
 
+                    if(!bWaitAll){
+                        return i;
+                    }
+                }
+
+            }
+
+            if(bWaitAll && nDone == nCount){
+                break;
+            }
+            usleep(10);
+        }
+
+        return nDone;
     }
 
     bool WaitForSingleObject(std::atomic<bool> &hHandle, int dwMilliseconds){
@@ -31,5 +86,7 @@ namespace Events{
         hHandle.store(false);
         return true;
     }
+
+
 
 };
