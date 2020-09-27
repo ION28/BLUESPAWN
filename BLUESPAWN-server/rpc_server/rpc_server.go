27050fb501f33654a10be50f7eded635b68f36a1
Jake Smith <jake@metactf.com>
2020-09-26 21:19:45 -0400

    Update RPC Server based on new definitions & methods

--- BLUESPAWN-server/rpc_server/rpc_server.go
+++ BLUESPAWN-server/rpc_server/rpc_server.go
@@ -13,8 +13,15 @@ const (
 	port = ":50052"
 )
 
-type server struct {
-	pb.UnimplementedBluespawnRPCServer
+type server struct {}
+
+func (s *server) Svc() *pb.BluespawnRPCService {
+	return &pb.BluespawnRPCService{
+		SendLogMessage: s.SendLogMessage,
+		RecordDetection: s.RecordDetection,
+		AddAssociation: s.AddAssociation,
+		UpdateCertainty: s.UpdateCertainty,
+	}
 }
 
 func (s *server) SendLogMessage(ctx context.Context, in *pb.LogMessage) (*pb.ResponseMessage, error) {
@@ -28,7 +35,7 @@ func (s *server) RecordDetection(ctx context.Context, in *pb.Detection) (*pb.Res
 }
 
 func (s *server) AddAssociation(ctx context.Context, in *pb.DetectionAssociation) (*pb.ResponseMessage, error) {
-	log.Printf("Received Association for Detection ID %d and ID %d with strength %f", in.GetDetection_Id(), in.GetAssociated_Id(), in.GetStrength())
+	log.Printf("Received Association for Detection ID %d and ID %d with strength %f", in.GetDetectionId(), in.GetAssociatedId(), in.GetStrength())
 	return &pb.ResponseMessage{Received: true, Success: true}, nil
 }
 
@@ -37,13 +44,18 @@ func (s *server) UpdateCertainty(ctx context.Context, in *pb.DetectionCertaintyU
 	return &pb.ResponseMessage{Received: true, Success: true}, nil
 }
 
+func createServer() *server {
+	s := &server{}
+	return s
+}
+
 func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterBluespawnRPCServer(s, &server{})
+	pb.RegisterBluespawnRPCService(s, createServer().Svc())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
