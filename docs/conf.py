d27b95cbce5c4cfd2357cd7848a828f62acd7a8e
Jake Smith <jake@metactf.com>
2021-04-14 19:59:04 -0400

    Work on importing docs into RTD

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ master_doc = 'index'
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -50,4 +50,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
