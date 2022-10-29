624b115cd89be238d6e62a1c9791dece81dc827b
Will Mayes <willmayes1999@gmail.com>
2022-10-28 17:59:54 -0700

    Revert "Removed check for behavior that bluespawn does not do"
    This reverts commit 4eec740d7af4a52f043b3dd17eab94074bb46540.

--- BLUESPAWN-win-client/test/Check/HuntT1547/Fixed/huntT1547-sub002-test001.ps1
+++ BLUESPAWN-win-client/test/Check/HuntT1547/Fixed/huntT1547-sub002-test001.ps1
@@ -1,4 +1,16 @@
 $passed = $true
+if (Test-Path "HKLM:\System\CurrentControlSet\Control\LSA"){
+    try {
+        $value = Get-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\LSA" -Name "Authentication Packages" -ErrorAction SilentlyContinue
+        if ($value.'Authentication Packages' -like "*T1547002001*"){
+            Write-Output "Hunt-T1547-Sub002-Test001: Registry value not removed"
+            $passed = $false;
+        }
+    } 
+    catch{
+        Write-Output "HI"
+    }
+}
 if (Test-Path "C:\Windows\System32\T1547002001.dll"){
     Write-Output "Hunt-T1547-Sub002-Test001: File not removed"
     $passed = $false;
